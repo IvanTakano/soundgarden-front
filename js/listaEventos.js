862114ab690b2bda347955f7e6f44136cd785954
João Menezes <joaosmenezes@hotmail.com>
2022-03-29 15:41:38 -0300

    criação do listaEventos.js

---
+++ js/listaEventos.js
@@ -0,0 +1,33 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+const optionEvents = {
+    method:"GET",
+    headers: {
+        "Content-Type": "application/json"
+    },
+};
+const table = document.querySelector(".table")
+const listarEventos = async () => {
+    const resposta = await fetch (`${BASE_URL}/events`, optionEvents)
+    const eventos = await resposta.json();
+    const htmlEventos = eventos.map((evento, index) => {
+        const dataEvento = new Date (evento.scheduled);
+        const linha = `<tr>
+            <th scope="row">${index+1}</th>
+                <td>${dataEvento.toLocaleDateString('en-GB')}</td>
+                <td>${evento.name}</td>
+                <td>${evento.description}</td>
+                <td>
+                    <a href="" class="btn btn-dark">ver reservas</a>
+                    <a href="editar-evento.html?id=${evento.id}" class="btn btn-secondary">editar</a>
+                    <a href="excluir-evento.html?id=${evento.id}" class="btn btn-danger">excluir</a>
+                </td>
+            </tr>
+        `
+        return linha
+    })
+    return htmlEventos
+}
+
+const eventos = listarEventos().then((resp) => {table.innerHTML += resp.slice
+(0,50)})
