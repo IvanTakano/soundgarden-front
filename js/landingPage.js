bda785bda3f40b06577b7daa87deeeafa5867b77
Ivan Yura Takano <ivanyurat@gmail.com>
2022-03-31 13:03:24 -0300

    listando os eventos na landingPage

--- js/landingPage.js
+++ js/landingPage.js
@@ -6,38 +6,28 @@ const optionEvents = {
         "Content-Type": "application/json"
     },
 };
-const table = document.querySelector(".evento.card.p-5.m-3")
+const table = document.querySelector("#card")
+
 const listarEventos = async () => {
-    const resposta = await fetch (`${BASE_URL}/booking`, optionEvents)
+    const resposta = await fetch (`${BASE_URL}/events`, optionEvents)
     const eventos = await resposta.json();
     const htmlEventos = eventos.map((evento) => {
         const dataEvento = new Date(evento.scheduled);
-        const linha = `<tr>
-            
-             <td>${evento.name}</td>
-                <td>${dataEvento.toLocaleDateString('en-GB')} ${dataEvento.toLocaleTimeString('en-GB').slice(0, -3)}</td>
-                <td>${evento.attractions}</td>
-                <td>${description}</td>
+        const linha = `
+          <div class="container d-flex justify-content-center align-items-center" >
+              <article class="evento card p-5 m-3" id="cardInterno"> 
+              <tr>
+          
+              <td>${evento.name} </br></td>
+              <td>${dataEvento.toLocaleDateString('en-GB')} </br></td>
+              <td>${evento.attractions}</br></td>
+              <td>${evento.description}</br></td>
 
-                    <a href="?id=${evento._id}" class="btn btn-dark">reservar ingressos</a>
-                <div id="myModal" class="modal">
-                        <div class="modal-content">
-                          <span class="close">&times;</span>
-                          <h5>Reserve seu ingresso</h5>
-                          <form class="modal-form">
-                              <div>
-                                <label for="modal-nome" class="modal-label">Nome </label>
-                                <input type="text" class="modal-form-control" id="modal-nome">
-                              </div>
-                              <div>
-                                <label for="modal-email" class="modal-label">E-mail</label>
-                                <input type="text" class="modal-form-control" id="modal-email">
-                              </div>
-                              <button type="submit" class="btn btn-primary" id="btn-modal-reservar">Reservar</button>
-                          </form>
-                        </div>                      
-                      </div>
-            </tr>
+              <a class="btn btn-dark " id="myBtn">reservar ingressos</a>
+              
+              </tr>
+              </article>
+          </div>                           
         `;
         return linha;
     })
