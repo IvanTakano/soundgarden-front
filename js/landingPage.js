768337f018ed8554b2d873c683d2ce9d57e32b95
Ivan Yura Takano <ivanyurat@gmail.com>
2022-04-01 09:36:39 -0300

    Ajustes modal

--- js/landingPage.js
+++ js/landingPage.js
@@ -7,14 +7,12 @@ const optionEvents = {
     },
 };
 const table = document.querySelector("#card")
-let idEvento = "";
 
 const listarEventos = async () => {
     const resposta = await fetch (`${BASE_URL}/events`, optionEvents)
     const eventos = await resposta.json();
     const htmlEventos = eventos.map((evento) => {
-        const dataEvento = new Date(evento.scheduled);
-        idEvento = evento.id;
+        const dataEvento = new Date(evento.scheduled);        
         const linha = `
           <div class="container d-flex justify-content-center align-items-center" >
               <article class="evento card p-5 m-3" id="cardInterno">               
@@ -24,11 +22,11 @@ const listarEventos = async () => {
               <p>${evento.attractions}</br></p>
               <p>${evento.description}</br></p>
 
-              <a class="btn btn-dark" id="myBtn">reservar ingressos</a>              
-              
+              <a class="btn btn-dark" id="myBtn" btn-id="${evento._id}">reservar ingressos</a>              
               </article>
           </div>                           
         `;
+        
         return linha;   
         
     })
@@ -36,19 +34,32 @@ const listarEventos = async () => {
 }
 
 const eventos = listarEventos().then((resp) => {table.innerHTML = resp.slice(0,3);
-  var modal = document.getElementById("myModal");
-  var btn = document.getElementById(`myBtn`);
-  var span = document.getElementsByClassName("close")[0];
-  console.log(modal);
+  const modal = document.getElementById("myModal");
+  const btn = document.getElementById(`myBtn`);
+  const span = document.getElementsByClassName("close")[0];  
+
   btn.onclick =(evento) => {    
-    evento.preventDefault();
-    console.log("Abrir modal")
+    evento.preventDefault();   
+    console.log("Abrir modal")  
+    let buttonsOpenModal = document.querySelectorAll("#myBtn");
     modal.style.display = "block";
-  };
+  }; 
 
   span.addEventListener ("click",() => {
     modal.style.display = "none";
-  })                  
+  })  
 });
+
+function adicionarEvento() {
+  let buttonsOpenModal = document.querySelectorAll("#myBtn");
+  buttonsOpenModal.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.querySelector("#myModal").style.display = "block";
+      listEventsById(btn.getAttribute("btn-id"));
+      
+    });
+  });
+}
+
 
 
